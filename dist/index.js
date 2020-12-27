61ccea2067503cc52a174e3405d809293ddd7bbf
Samuel Bangslund <s.bangslund@hotmail.dk>
2020-12-27 17:04:31 +0100

    Back to typescript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayGoodbye = exports.sayHello = void 0;
+var engine_1 = require("./engine");
+Object.defineProperty(exports, "sayHello", { enumerable: true, get: function () { return engine_1.sayHello; } });
+Object.defineProperty(exports, "sayGoodbye", { enumerable: true, get: function () { return engine_1.sayGoodbye; } });
