70a9238e839b941aee3505c5d8919bccee0b2bef
Samuel Bangslund <s.bangslund@hotmail.dk>
2020-12-30 21:07:30 +0100

    Fixed files

--- tests/engine.test.ts
+++ tests/engine.test.ts
@@ -2,13 +2,15 @@ import { ECSEngine } from "../src/engine";
 import { ECSWorld } from "../src/world";
 
 let world: ECSWorld;
+let engine: ECSEngine;
 
 let sum = (a, b) => {
     return a + b;
 }
 
 beforeAll(() => {
-    world = new ECSWorld('nodes.json');
+    world = new ECSWorld('tests/files/nodes.json');
+    engine = new ECSEngine(world, 'tests/files/systems.json', 'tests/files/plugins');
 });
 
 test('Help me!', () => {
